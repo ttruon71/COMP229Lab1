69484e1f531363f61aeb2c2ca54e8c88d3c3873b
Thomas Truong <ttruon71@my.centennialcollege.ca>
2024-10-01 21:25:03 -0400

    Create About Me Page

---
+++ portfolio/src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+import Self from '../../Assets/Self.jpg'
+
+const About = () => {
+    return(
+        <section id='about'>
+            <span className='AboutPara'>Hi I'm Thomas Truong,<br /> I live in Ontario and I'm a full-time student at Centennial College<br />I enjoy reading and going to the gym<br /><a href="../../Assets/ThomasTruong_Resume.pdf" target='_blank' rel='noopener noreferrer'>Here</a> you can find my resume</span>
+            <div>
+                <img src={Self} alt="Self Picture" className="Self" />
+            </div>
+        </section>
+    );
+}
+
+export default About
