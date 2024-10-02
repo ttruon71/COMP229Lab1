b6e006c57bc111b9c71f9d76bd5e710d16c875f4
Thomas Truong <ttruon71@my.centennialcollege.ca>
2024-10-01 20:46:07 -0400

    Create Home Page Section (Name, Mission Statement, etc.)

---
+++ portfolio/src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <section id="intro">
+            <div className="intro">
+                <span className="hello">Hello,</span>
+                <span className="name">I'm Thomas Truong,<br/>Centennial College Student</span>
+                <p className="missionStatement">I am currently a full-time student at Cetennial College and I'm in my 3rd semester<br />I'm hoping to learn a lot from this web development class and take those skills into the real world.</p>
+            </div>
+        </section>
+    )
+}
+
+export default HomePage
